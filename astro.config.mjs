311b2e75836d272d07dbd5cc2adf8cf6720e3b83
Yewolf <yyewolf@gmail.com>
2025-09-25 08:33:41 -0400

    feat: add matomo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,27 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import matomo from 'astro-matomo';
 
 export default defineConfig({
   site: "https://yewolf.fr",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [
+    mdx(), 
+    sitemap(), 
+    tailwind(),
+    matomo({
+      enabled: true,
+      host: "https://analytics.yewolf.fr/",
+      setCookieDomain: "*.yewolf.fr",
+      trackerUrl: "matomo.php",
+      srcUrl: "matomo.js",
+      siteId: 1,
+      heartBeatTimer: 5,
+      disableCookies: true,
+      debug: false,
+      viewTransition: {
+        contentElement: "main"
+      }
+    }),
+  ],
 });
