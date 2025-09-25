c0a9ed2d6c193aaff990641780ae48b8649fc0bd
Yewolf <yyewolf@gmail.com>
2025-09-25 02:13:44 -0400

    fix: url

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,9 @@ import type { Site, Metadata, Socials } from "@types";
 export const SITE: Site = {
   NAME: "Yewolf",
   EMAIL: "contact@yewolf.fr",
-  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 4,
   NUM_WORKS_ON_HOMEPAGE: 2,
-  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_PROJECTS_ON_HOMEPAGE: 4,
 };
 
 export const HOME: Metadata = {
